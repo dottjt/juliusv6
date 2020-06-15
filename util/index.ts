631ffefad3c7663bf3266701b800180d34f58d95
dottjt <julius.reade@myob.com>
2020-06-15 13:22:20 +1000

    automated datareade update push

--- util/index.ts
+++ util/index.ts
@@ -1,15 +1,32 @@
 import path from 'path';
-import { data, website } from '@dottjt/datareade';
+import { data, website, media } from '@dottjt/datareade';
 
 const main = async () => {
-  const { projects } = data;
+  const { projects, feedNFDSocial } = data;
   const { generateHugoMDFilesV6 } = website;
+  const { generateRSSFeed } = media;
 
   const contentDirectory = path.join(__dirname, '..', 'content');
+
+  // GENERATE WEBSITE CONTENT
 
   await generateHugoMDFilesV6({
     projects,
     contentDirectory,
+  });
+
+  // GENERATE RSS FEEDS
+
+  const OUTPUT_RSS_FILE = path.join(__dirname, '..', 'themes', 'reade', 'static', 'nfd_social_feed.xml');
+
+  await generateRSSFeed({
+    title: 'NeverFap Deluxe Social Feed',
+    websiteLink: 'https://neverfapdeluxe.com/',
+    description: 'NeverFap Deluxe Social Feed so I can aggregate to the world. Something along those lines, I think.',
+
+    linkRSSFile: 'https://juliusreade.com/nfd_social_feed.xml',
+    outputRSSFile: OUTPUT_RSS_FILE,
+    feedList: feedNFDSocial
   });
 };
 
