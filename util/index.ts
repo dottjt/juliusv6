a51742ab55c4d02a99aeb6f0f0b37e5f88423428
dottjt <julius.reade@myob.com>
2020-06-19 16:56:22 +1000

    yus

--- util/index.ts
+++ util/index.ts
@@ -14,20 +14,6 @@ const main = async () => {
     projects,
     contentDirectory,
   });
-
-  // GENERATE RSS FEEDS
-
-  const OUTPUT_RSS_FILE = path.join(__dirname, '..', 'themes', 'reade', 'static', 'nfd_social_feed.xml');
-
-  await generateRSSFeed({
-    title: 'NeverFap Deluxe Social Feed',
-    websiteLink: 'https://neverfapdeluxe.com/',
-    description: 'NeverFap Deluxe Social Feed so I can aggregate to the world. Something along those lines, I think.',
-
-    linkRSSFile: 'https://juliusreade.com/nfd_social_feed.xml',
-    outputRSSFile: OUTPUT_RSS_FILE,
-    feedList: feedNFDSocial
-  });
 };
 
 main();
